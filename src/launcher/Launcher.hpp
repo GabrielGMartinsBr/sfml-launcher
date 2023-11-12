4879bb92706b4109262cc68111de7998d8f01498
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-11-12 16:41:53 -0300

    Move frame rate control to new LauncherTimer class

--- src/launcher/Launcher.hpp
+++ src/launcher/Launcher.hpp
@@ -2,14 +2,12 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
-#include <chrono>
-#include <cmath>
 #include <iostream>
 #include <thread>
 
-#include "base/Log.hpp"
 #include "graphics/Graphics.hpp"
 #include "integrator/Integrator.hpp"
+#include "launcher/LauncherTimer.hpp"
 
 class Launcher {
   int width = 600;
@@ -19,11 +17,7 @@ class Launcher {
   sf::VideoMode mode;
   sf::RenderWindow window;
 
-  float elapsed;
-  float fps;
-  std::chrono::high_resolution_clock::time_point currentTime;
-  std::chrono::high_resolution_clock::time_point previousTime;
-  float frameTime = 1.0f / 60.0f * 1e6;
+  LauncherTimer timer;
 
   Integrator integrator;
   Graphics gr;
@@ -41,20 +35,16 @@ class Launcher {
     std::thread rbThread(integrator);
 
     while (window.isOpen()) {
-      currentTime = std::chrono::high_resolution_clock::now();
-      elapsed = (float)std::chrono::duration_cast<std::chrono::microseconds>(currentTime - previousTime).count();
+      timer.update();
 
-      if (elapsed >= frameTime) {
+      if (timer.shouldUpdate()) {
         listenEvents();
         update();
         render();
-
-        fps = (float)1e6 / elapsed;
-        Log::out() << "FPS: " << std::floor(fps);
-
-        previousTime = currentTime;
+        timer.markUpdate();
+        // timer.printFps();
       } else {
-        std::this_thread::sleep_for(std::chrono::milliseconds(3));
+        timer.sleep();
       }
     }
 
