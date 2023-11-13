6854731062874d80672621b4bf84c7d0c012f10f
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-11-13 00:14:26 -0300

    Add todos

--- src/engnine/Graphics.hpp
+++ src/engnine/Graphics.hpp
@@ -11,20 +11,26 @@ class Graphics {
   Graphics& operator=(const Graphics&);
 
  public:
+  unsigned int frame_rate = 40;
+  unsigned long frame_count = 0;
+
   static Graphics& getInstance()
   {
     static Graphics instance;
     return instance;
   }
 
-  unsigned int frame_rate = 40;
-  unsigned long frame_count = 0;
-
   void update()
   {
     Engine::getInstance().updateGraphics();
     frame_count++;
   }
+
+  void freeze();  // TODO: Implement this method
+
+  void transition();  // TODO: Implement this method
+
+  void frame_reset();  // TODO: Implement this method
 };
 
 }  // namespace Eng
