015f288dbc90aa32474867d2776a0aeaa41fc30e
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2023-11-19 01:24:18 -0300

    Remove deprecated sprites and viewports render

--- src/engnine/Engine.hpp
+++ src/engnine/Engine.hpp
@@ -10,10 +10,8 @@
 #include <stdexcept>
 #include <vector>
 
-#include "base/Log.hpp"
 #include "base/Sugars.hpp"
 #include "engnine/EngineRenderer.hpp"
-#include "engnine/RGSSViewport.hpp"
 #include "engnine/Sprite.hpp"
 #include "ruby.h"
 
@@ -50,27 +48,9 @@ class Engine {
     return running;
   }
 
-  /*
-    Viewports
-  */
-
-  RGSS::Viewport& getDefaultViewport()
-  {
-    return defaultViewport;
-  }
-
   void addViewport(SharedPtr<Eng::Viewport> vp)
   {
     renderer->addViewport(vp);
-  }
-
-  /*
-    Sprites
-  */
-
-  std::vector<VALUE>* getSprites()
-  {
-    return &sprites;
   }
 
   void addSprite(VALUE spriteValue)
@@ -78,10 +58,6 @@ class Engine {
     Eng::Sprite* spr = (Eng::Sprite*)DATA_PTR(spriteValue);
     renderer->addSprite(spr);
   }
-
-  /*
-    Others
-  */
 
   void updateInput()
   {
@@ -102,60 +78,15 @@ class Engine {
   bool initialized = false;
   bool running = false;
 
-  RGSS::Viewport defaultViewport;
-  Vector<SharedPtr<Eng::Viewport>> viewports;
-  Vector<VALUE> sprites;
-
   sf::RenderWindow* window = nullptr;
   EngineRenderer* renderer = nullptr;
 
-  Engine() :
-      defaultViewport(0, 0, 640, 480)
+  Engine()
   {
   }
 
   Engine(const Engine&);
   Engine& operator=(const Engine&);
-
-  void renderViewports()
-  {
-    defaultViewport.clear();
-    for (SharedPtr<Eng::Viewport> vp : viewports) {
-      if (vp) {
-        vp->getRgssViewport().clear();
-      }
-    }
-
-    updateSprites();
-
-    defaultViewport.renderIn(*window);
-    defaultViewport.display();
-
-    for (SharedPtr<Eng::Viewport> vp : viewports) {
-      if (vp) {
-        // defaultViewport.draw(
-        //   vp->getRgssViewport().sprite
-        // );
-        vp->getRgssViewport().renderIn(*window);
-        // vp->getRgssViewport().renderIn(defaultViewport);
-        vp->getRgssViewport().display();
-      }
-    }
-
-    // defaultViewport.renderIn(*window);
-    // defaultViewport.display();
-  }
-
-  void updateSprites()
-  {
-    Eng::Sprite* spr = nullptr;
-    for (VALUE i : sprites) {
-      spr = (Eng::Sprite*)DATA_PTR(i);
-      if (spr) {
-        spr->renderInViewport(defaultViewport);
-      }
-    }
-  }
 
   void pollEvents()
   {
