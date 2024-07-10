82cfffa7d8327d6d25e64a3818e2193a11a2b7e5
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-07-10 05:59:42 -0300

    Fix missing define once macro

--- src/base/MarshalUtils.hpp
+++ src/base/MarshalUtils.hpp
@@ -1,4 +1,7 @@
+#pragma once
+
 #include <cstring>
+
 struct MarshalUtils {
   static inline int readInt32(const char **data)
   {
@@ -6,6 +9,16 @@ struct MarshalUtils {
 
     std::memcpy(&result, *data, 4);
     *data += 4;
+
+    return result;
+  }
+
+  static inline int readDouble(const char **data)
+  {
+    double result;
+
+    std::memcpy(&result, *data, 8);
+    *data += 8;
 
     return result;
   }
