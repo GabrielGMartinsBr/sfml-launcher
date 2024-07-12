5905b23533380a7b024dc50258f98717cc004cfa
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-07-12 03:31:51 -0300

    Add more Audio fns definitions

--- src/engnine/Audio.hpp
+++ src/engnine/Audio.hpp
@@ -7,14 +7,52 @@ namespace Eng {
 class Audio {
 
  public:
+  // BGM
+
   static void bgm_play()
   {
-    Log::out() << "Play bgm was called, but it is not implemented yet.";
+    Log::out() << "Audio bgm_play was called, but it is not implemented yet.";
   }
 
   static void bgm_stop()
   {
-    Log::out() << "Stop bgm was called, but it is not implemented yet.";
+    Log::out() << "Audio bgm_stop was called, but it is not implemented yet.";
+  }
+
+  // BGS
+
+  static void bgs_play()
+  {
+    Log::out() << "Audio bgs_play was called, but it is not implemented yet.";
+  }
+
+  static void bgs_stop()
+  {
+    Log::out() << "Audio bgs_stop was called, but it is not implemented yet.";
+  }
+
+  // ME
+
+  static void me_play()
+  {
+    Log::out() << "Audio me_play was called, but it is not implemented yet.";
+  }
+
+  static void me_stop()
+  {
+    Log::out() << "Audio me_stop was called, but it is not implemented yet.";
+  }
+
+  // SE
+
+  static void se_play()
+  {
+    Log::out() << "Audio se_play was called, but it is not implemented yet.";
+  }
+
+  static void se_stop()
+  {
+    Log::out() << "Audio se_stop was called, but it is not implemented yet.";
   }
 };
 }  // namespace Eng
--- src/integrator/It_Audio.hpp
+++ src/integrator/It_Audio.hpp
@@ -16,9 +16,23 @@ class Audio {
 
     rb_define_module_function(audioModule, "bgm_play", RUBY_METHOD_FUNC(bgm_play), -1);
     rb_define_module_function(audioModule, "bgm_stop", RUBY_METHOD_FUNC(bgm_stop), 0);
+
+    rb_define_module_function(audioModule, "bgs_play", RUBY_METHOD_FUNC(bgs_play), -1);
+    rb_define_module_function(audioModule, "bgs_stop", RUBY_METHOD_FUNC(bgs_stop), 0);
+
+    rb_define_module_function(audioModule, "me_play", RUBY_METHOD_FUNC(me_play), -1);
+    rb_define_module_function(audioModule, "me_stop", RUBY_METHOD_FUNC(me_stop), 0);
+
+    rb_define_module_function(audioModule, "se_play", RUBY_METHOD_FUNC(se_play), -1);
+    rb_define_module_function(audioModule, "se_stop", RUBY_METHOD_FUNC(se_stop), 0);
   }
 
  private:
+
+  /*
+    BGM
+  */
+
   static VALUE bgm_play(int argc, VALUE *argv, VALUE self)
   {
     VALUE _filename, _volume, _pitch;
@@ -48,6 +62,114 @@ class Audio {
   static VALUE bgm_stop(VALUE self)
   {
     Eng::Audio::bgm_stop();
+    return Qnil;
+  }
+
+  /*
+    BGS
+  */
+
+  static VALUE bgs_play(int argc, VALUE *argv, VALUE self)
+  {
+    VALUE _filename, _volume, _pitch;
+    switch (argc) {
+      case 1: {
+        rb_scan_args(argc, argv, "1", &_filename);
+        Eng::Audio::bgs_play();
+        break;
+      }
+      case 2: {
+        rb_scan_args(argc, argv, "2", &_filename, &_volume);
+        Eng::Audio::bgs_play();
+        break;
+      }
+      case 3: {
+        rb_scan_args(argc, argv, "3", &_filename, &_volume, &_pitch);
+        Eng::Audio::bgs_play();
+        break;
+      }
+      default: {
+        throw std::runtime_error("Audio.bgm_play takes 1 to 3 arguments, but were received " + std::to_string(argc));
+      }
+    }
+    return Qnil;
+  }
+
+  static VALUE bgs_stop(VALUE self)
+  {
+    Eng::Audio::bgs_stop();
+    return Qnil;
+  }
+
+  /*
+    ME
+  */
+
+  static VALUE me_play(int argc, VALUE *argv, VALUE self)
+  {
+    VALUE _filename, _volume, _pitch;
+    switch (argc) {
+      case 1: {
+        rb_scan_args(argc, argv, "1", &_filename);
+        Eng::Audio::me_play();
+        break;
+      }
+      case 2: {
+        rb_scan_args(argc, argv, "2", &_filename, &_volume);
+        Eng::Audio::me_play();
+        break;
+      }
+      case 3: {
+        rb_scan_args(argc, argv, "3", &_filename, &_volume, &_pitch);
+        Eng::Audio::me_play();
+        break;
+      }
+      default: {
+        throw std::runtime_error("Audio.bgm_play takes 1 to 3 arguments, but were received " + std::to_string(argc));
+      }
+    }
+    return Qnil;
+  }
+
+  static VALUE me_stop(VALUE self)
+  {
+    Eng::Audio::me_stop();
+    return Qnil;
+  }
+
+  /*
+    SE
+  */
+
+  static VALUE se_play(int argc, VALUE *argv, VALUE self)
+  {
+    VALUE _filename, _volume, _pitch;
+    switch (argc) {
+      case 1: {
+        rb_scan_args(argc, argv, "1", &_filename);
+        Eng::Audio::se_play();
+        break;
+      }
+      case 2: {
+        rb_scan_args(argc, argv, "2", &_filename, &_volume);
+        Eng::Audio::se_play();
+        break;
+      }
+      case 3: {
+        rb_scan_args(argc, argv, "3", &_filename, &_volume, &_pitch);
+        Eng::Audio::se_play();
+        break;
+      }
+      default: {
+        throw std::runtime_error("Audio.bgm_play takes 1 to 3 arguments, but were received " + std::to_string(argc));
+      }
+    }
+    return Qnil;
+  }
+
+  static VALUE se_stop(VALUE self)
+  {
+    Eng::Audio::se_stop();
     return Qnil;
   }
 };
