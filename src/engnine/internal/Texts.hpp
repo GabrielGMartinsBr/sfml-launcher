0538bed464320da4e76eafecd680cafcc6430ca6
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-07-25 01:47:08 -0300

    Add utf8 support to text render

--- src/engnine/internal/Texts.hpp
+++ src/engnine/internal/Texts.hpp
@@ -5,8 +5,11 @@
 #include <SFML/Graphics/Font.hpp>
 #include <SFML/Graphics/RenderTexture.hpp>
 #include <SFML/Graphics/Text.hpp>
+#include <SFML/System/String.hpp>
+#include <SFML/System/Utf.hpp>
 #include <stdexcept>
 
+#include "Log.hpp"
 #include "base/AppDefs.h"
 #include "engnine/Font.hpp"
 
@@ -35,12 +38,14 @@ class Texts {
 
   static void drawText(sf::RenderTexture &texture, int x, int y, Eng::Font &font, const app::CStr &str)
   {
+    sf::String s = fromUtf8(str);
     sf::Font sfFont;
     loadFont(sfFont);
     sfFont.setSmooth(false);
     sf::Text text;
     text.setFont(sfFont);
-    text.setString(str);
+    // text.setString(str);
+    text.setString(s);
     text.setCharacterSize(font.getter_size());
     text.setFillColor(font.getter_color()->getSfColor());
     text.setPosition(x, y);
@@ -48,6 +53,20 @@ class Texts {
     // text.setOutlineThickness(.5);
     // texture.setSmooth(true);
     texture.draw(text, sf::BlendNone);
+  }
+
+  static sf::String fromUtf8(const std::string &in)
+  {
+    std::basic_string<sf::Uint32> tmp;
+    sf::Utf8::toUtf32(in.begin(), in.end(), back_inserter(tmp));
+    return sf::String(tmp);
+  }
+
+  static std::string toUtf8(const sf::String &str)
+  {
+    std::string out;
+    sf::Utf32::toUtf8(str.begin(), str.end(), back_inserter(out));
+    return out;
   }
 };
 
