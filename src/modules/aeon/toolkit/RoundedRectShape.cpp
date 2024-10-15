5ee43ae8f656a69bfdde8d3bf7ae05fffc18fc57
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-10-15 03:55:31 -0300

    feat: Add Aeon Toolkit RoundedRectShape

---
+++ src/modules/aeon/toolkit/RoundedRectShape.cpp
@@ -0,0 +1,290 @@
+#include "./RoundedRectShape.h"
+
+#include <cmath>
+
+namespace ae {
+
+/*
+  ------------------------------------------------------
+
+    ⇩⇩⇩⇩⇩⇩⇩⇩⇩ Public ⇩⇩⇩⇩⇩⇩⇩⇩⇩
+
+  ------------------------------------------------------
+*/
+
+RoundedRectShape::RoundedRectShape(const Rect& bounds, float radius) :
+    RoundedRectShape(
+      bounds.x.get(),
+      bounds.y.get(),
+      bounds.width.get(),
+      bounds.height.get(),
+      radius
+    )
+{
+}
+
+RoundedRectShape::RoundedRectShape(float x, float y, float width, float height, float radius) :
+    positionVal(x, y),
+    sizeVal(width, height),
+    dirtyStyle(false),
+    dirtyPosition(false),
+    dirtyPoints(false)
+{
+  style.radius = radius;
+  setPoints();
+  setPosition();
+  setStyle();
+}
+
+void RoundedRectShape::drawTo(RenderTarget& target)
+{
+  refreshValues();
+  target.draw(shape);
+}
+
+/*
+  Position
+*/
+
+const Vector2f& RoundedRectShape::position() const
+{
+  return positionVal;
+}
+
+void RoundedRectShape::position(float x, float y)
+{
+  positionVal.x = x;
+  positionVal.y = y;
+  dirtyPosition = true;
+}
+
+void RoundedRectShape::position(const Vector2f& value)
+{
+  positionVal = value;
+  dirtyPosition = true;
+}
+
+float RoundedRectShape::x()
+{
+  return positionVal.x;
+}
+
+void RoundedRectShape::x(float value)
+{
+  dirtyPosition = true;
+}
+
+float RoundedRectShape::y()
+{
+  return positionVal.y;
+}
+
+void RoundedRectShape::y(float value)
+{
+  dirtyPosition = true;
+}
+
+/*
+  Size
+*/
+
+const Vector2f& RoundedRectShape::size() const
+{
+  return sizeVal;
+}
+
+void RoundedRectShape::size(float width, float height)
+{
+  sizeVal.x = width;
+  sizeVal.y = height;
+  dirtyPoints = true;
+}
+
+void RoundedRectShape::size(const Vector2f& value)
+{
+  sizeVal = value;
+  dirtyPoints = true;
+}
+
+float RoundedRectShape::width()
+{
+  return sizeVal.x;
+}
+
+void RoundedRectShape::width(float value)
+{
+  dirtyPoints = true;
+}
+
+float RoundedRectShape::height()
+{
+  return sizeVal.y;
+}
+
+void RoundedRectShape::height(float value)
+{
+  dirtyPoints = true;
+}
+
+/*
+  Bounds
+*/
+
+// const Rect& RoundedRectShape::bounds() const { }
+
+void RoundedRectShape::bounds(float x, float y, float width, float height)
+{
+  positionVal.x = x;
+  positionVal.y = y;
+  sizeVal.x = width;
+  sizeVal.y = height;
+  dirtyPosition = true;
+  dirtyPoints = true;
+}
+
+void RoundedRectShape::bounds(const Vector2f& position, const Vector2f& size)
+{
+  positionVal = position;
+  sizeVal = size;
+  dirtyPosition = true;
+  dirtyPoints = true;
+}
+
+void RoundedRectShape::bounds(const Rect& value)
+{
+  positionVal.x = value.x.get();
+  positionVal.y = value.y.get();
+  sizeVal.x = value.width.get();
+  sizeVal.y = value.height.get();
+  dirtyPosition = true;
+  dirtyPoints = true;
+}
+
+/*
+  Rounded radius
+*/
+
+float RoundedRectShape::radius()
+{
+  return style.radius;
+}
+
+void RoundedRectShape::radius(float radius)
+{
+  style.radius = radius;
+  markDirtyPoints();
+}
+
+/*
+  Mark dirty flags
+*/
+
+void RoundedRectShape::markDirtyStyle()
+{
+  dirtyStyle = true;
+}
+
+void RoundedRectShape::markDirtyPoints()
+{
+  dirtyPoints = true;
+}
+
+/*
+  ------------------------------------------------------
+
+    ⇩⇩⇩⇩⇩⇩⇩⇩⇩ Private ⇩⇩⇩⇩⇩⇩⇩⇩⇩
+
+  ------------------------------------------------------
+*/
+
+void RoundedRectShape::refreshValues()
+{
+  if (dirtyStyle) {
+    setStyle();
+    dirtyStyle = false;
+  }
+  if (dirtyPosition) {
+    setPosition();
+    dirtyPosition = false;
+  }
+  if (dirtyPoints) {
+    setPoints();
+    dirtyPoints = false;
+  }
+}
+
+void RoundedRectShape::setPosition()
+{
+  shape.setPosition(positionVal);
+}
+
+void RoundedRectShape::setStyle()
+{
+  shape.setOutlineThickness(style.borderSize);
+  shape.setOutlineColor(style.bgColor.getSfColor());
+  shape.setFillColor(style.bgColor.getSfColor());
+}
+
+void RoundedRectShape::setPoints()
+{
+  int points = 16;
+  float radius = style.radius;
+  Vector2f c(0, 0);
+  int point = 0;
+  float start = M_PI * 0.5;
+  float end = M_PI * 0;
+
+  shape.setPointCount(points * 4);
+
+  for (int i = 0; i < points; i++) {
+    float angle = start + (i * (end - start)) / points;
+    float rad = angle * M_PI / 180.0f;
+    float dx = std::cos(angle) * radius;
+    float dy = std::sin(angle) * radius;
+    c.x = sizeVal.x - radius + dx;
+    c.y = radius - dy;
+    shape.setPoint(point, Vector2f(c));
+    point++;
+  }
+
+  start = M_PI * 2;
+  end = M_PI * 1.5;
+  for (int i = 0; i < points; i++) {
+    float angle = start + (i * (end - start)) / points;
+    float rad = angle * M_PI / 180.0f;
+    float dx = std::cos(angle) * radius;
+    float dy = std::sin(angle) * radius;
+    c.x = sizeVal.x - radius + dx;
+    c.y = sizeVal.y - radius - dy;
+    shape.setPoint(point, Vector2f(c));
+    point++;
+  }
+
+  start = M_PI * 1.5;
+  end = M_PI * 1;
+  for (int i = 0; i < points; i++) {
+    float angle = start + (i * (end - start)) / points;
+    float rad = angle * M_PI / 180.0f;
+    float dx = std::cos(angle) * radius;
+    float dy = std::sin(angle) * radius;
+    c.x = radius + dx;
+    c.y = sizeVal.y - radius - dy;
+    shape.setPoint(point, Vector2f(c));
+    point++;
+  }
+
+  start = M_PI * 1;
+  end = M_PI * 0.5;
+  for (int i = 0; i < points; i++) {
+    float angle = start + (i * (end - start)) / points;
+    float rad = angle * M_PI / 180.0f;
+    float dx = std::cos(angle) * radius;
+    float dy = std::sin(angle) * radius;
+    c.x = radius + dx;
+    c.y = radius - dy;
+    shape.setPoint(point, Vector2f(c));
+    point++;
+  }
+}
+
+}  // namespace ae
---
+++ src/modules/aeon/toolkit/RoundedRectShape.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <SFML/Graphics/ConvexShape.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+#include "engnine/Rect.hpp"
+#include "modules/aeon/window/AeonStylesheet.h"
+
+namespace ae {
+
+using Eng::Rect;
+using sf::ConvexShape;
+using sf::RenderTarget;
+using sf::Vector2f;
+
+struct RoundedRectShape {
+  AeonStylesheet style;
+
+  RoundedRectShape(const Rect& bounds, float radius = 0);
+  RoundedRectShape(float x, float y, float width, float height, float radius = 0);
+
+  void drawTo(RenderTarget& target);
+
+  const Vector2f& position() const;
+  void position(float x, float y);
+  void position(const Vector2f& value);
+
+  float x();
+  void x(float value);
+  float y();
+  void y(float value);
+
+  const Vector2f& size() const;
+  void size(float width, float height);
+  void size(const Vector2f& value);
+
+  float width();
+  void width(float value);
+  float height();
+  void height(float value);
+
+  // const Rect& bounds() const;
+  void bounds(float x, float y, float width, float height);
+  void bounds(const Vector2f& position, const Vector2f& size);
+  void bounds(const Rect& value);
+
+  float radius();
+  void radius(float radius);
+
+  void markDirtyStyle();
+  void markDirtyPoints();
+
+ private:
+  Vector2f positionVal;
+  Vector2f sizeVal;
+  ConvexShape shape;
+  bool dirtyStyle;
+  bool dirtyPosition;
+  bool dirtyPoints;
+
+  void refreshValues();
+  void setPoints();
+  void setPosition();
+  void setStyle();
+};
+
+}  // namespace ae
