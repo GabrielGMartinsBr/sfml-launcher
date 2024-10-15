933ae6a95172cdf55d2caccb33d23215aa909978
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-10-15 08:31:10 -0300

    feat: Add ElementBounds vector class to Aeon Toolkit

---
+++ src/modules/aeon/toolkit/ElementBounds.cpp
@@ -0,0 +1,259 @@
+#include "aeon/toolkit/ElementBounds.h"
+
+namespace ae {
+
+/*
+  ⇩⇩⇩ Constructor ⇩⇩⇩
+*/
+
+ElementBounds::ElementBounds(float x, float y, float width, float height) :
+    xValue(x),
+    yValue(y),
+    wValue(width),
+    hValue(height),
+    xEndValue(x + width),
+    yEndValue(y + height)
+{
+}
+
+/*
+  ⇩⇩⇩ Getters ands Setters ⇩⇩⇩
+*/
+
+float ElementBounds::x() const
+{
+  return xValue;
+}
+
+float ElementBounds::x(float value)
+{
+  xValue = value;
+  xEndValue = xValue + wValue;
+  positionValue.x = xValue;
+  return xValue;
+}
+
+float ElementBounds::y() const
+{
+  return yValue;
+}
+
+float ElementBounds::y(float value)
+{
+  yValue = value;
+  yEndValue = yValue + hValue;
+  positionValue.y = yValue;
+  return yValue;
+}
+
+float ElementBounds::w() const
+{
+  return wValue;
+}
+
+float ElementBounds::w(float value)
+{
+  wValue = value;
+  xEndValue = xValue + wValue;
+  sizeValue.x = wValue;
+  return wValue;
+}
+
+float ElementBounds::h() const
+{
+  return hValue;
+}
+
+float ElementBounds::h(float value)
+{
+  hValue = value;
+  yEndValue = yValue + hValue;
+  sizeValue.y = hValue;
+  return hValue;
+}
+
+float ElementBounds::width() const
+{
+  return w();
+}
+
+float ElementBounds::width(float value)
+{
+  return w(value);
+}
+
+float ElementBounds::height() const
+{
+  return h();
+}
+
+float ElementBounds::height(float value)
+{
+  return h(value);
+}
+
+const Vector2f& ElementBounds::position() const
+{
+  return positionValue;
+}
+
+const Vector2f& ElementBounds::position(const Vector2f& value)
+{
+  positionValue = value;
+  xValue = positionValue.x;
+  yValue = positionValue.y;
+  xEndValue = xValue + wValue;
+  yEndValue = yValue + hValue;
+  return positionValue;
+}
+
+const Vector2f& ElementBounds::size() const
+{
+  return sizeValue;
+}
+
+const Vector2f& ElementBounds::size(const Vector2f& value)
+{
+  sizeValue = value;
+  wValue = sizeValue.x;
+  hValue = sizeValue.y;
+  xEndValue = xValue + wValue;
+  yEndValue = yValue + hValue;
+  return sizeValue;
+}
+
+/*
+  ⇩⇩⇩ Methods ⇩⇩⇩
+*/
+
+inline bool ElementBounds::isEmpty() const
+{
+  return wValue == 0 || hValue == 0;
+}
+
+inline bool ElementBounds::intersects(float x, float y) const
+{
+  return !isEmpty() && x >= xValue && x <= xEndValue && y >= yValue && y < yEndValue;
+}
+
+/*
+  ⇩⇩⇩ Operators ⇩⇩⇩
+*/
+
+inline bool ElementBounds::operator==(const ElementBounds& other) const
+{
+  return xValue == other.xValue && yValue == other.yValue
+         && wValue == other.wValue && hValue == other.hValue;
+}
+
+ElementBounds& ElementBounds::operator=(const ElementBounds& other)
+{
+  if (this == &other) {
+    return *this;
+  }
+
+  xValue = other.xValue;
+  yValue = other.yValue;
+  wValue = other.wValue;
+  hValue = other.hValue;
+  recalculateBounds();
+  return *this;
+}
+
+ElementBounds& ElementBounds::operator+=(const ElementBounds& other)
+{
+  xValue += other.xValue;
+  yValue += other.yValue;
+  wValue += other.wValue;
+  hValue += other.hValue;
+  recalculateBounds();
+  return *this;
+}
+
+ElementBounds& ElementBounds::operator-=(const ElementBounds& other)
+{
+  xValue -= other.xValue;
+  yValue -= other.yValue;
+  wValue -= other.wValue;
+  hValue -= other.hValue;
+  recalculateBounds();
+  return *this;
+}
+
+ElementBounds ElementBounds::operator+(const ElementBounds& other) const
+{
+  return ElementBounds(
+    xValue + other.xValue,
+    yValue + other.yValue,
+    wValue + other.wValue,
+    hValue + other.hValue
+  );
+}
+
+ElementBounds ElementBounds::operator-(const ElementBounds& other) const
+{
+  return ElementBounds(
+    xValue - other.xValue,
+    yValue - other.yValue,
+    wValue - other.wValue,
+    hValue - other.hValue
+  );
+}
+
+ElementBounds& ElementBounds::operator+=(float value)
+{
+  xValue += value;
+  yValue += value;
+  wValue += value;
+  hValue += value;
+  recalculateBounds();
+  return *this;
+}
+
+ElementBounds& ElementBounds::operator-=(float value)
+{
+  xValue -= value;
+  yValue -= value;
+  wValue -= value;
+  hValue -= value;
+  recalculateBounds();
+  return *this;
+}
+
+ElementBounds ElementBounds::operator+(float value) const
+{
+  return ElementBounds(
+    xValue + value,
+    yValue + value,
+    wValue + value,
+    hValue + value
+  );
+}
+
+ElementBounds ElementBounds::operator-(float value) const
+{
+  return ElementBounds(
+    xValue - value,
+    yValue - value,
+    wValue - value,
+    hValue - value
+  );
+}
+
+/*
+  ⇩⇩⇩ Private ⇩⇩⇩
+*/
+
+void ElementBounds::recalculateBounds()
+{
+  xEndValue = xValue + wValue;
+  yEndValue = yValue + hValue;
+
+  positionValue.x = xValue;
+  positionValue.y = yValue;
+
+  sizeValue.x = wValue;
+  sizeValue.y = hValue;
+}
+
+}  // namespace ae
---
+++ src/modules/aeon/toolkit/ElementBounds.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <SFML/System/Vector2.hpp>
+
+namespace ae {
+
+using sf::Vector2f;
+
+struct ElementBounds {
+  ElementBounds(float x = 0, float y = 0, float width = 0, float height = 0);
+
+  float x() const;
+  float x(float value);
+  float y() const;
+  float y(float value);
+
+  float w() const;
+  float w(float value);
+  float h() const;
+  float h(float value);
+
+  float width() const;
+  float width(float value);
+  float height() const;
+  float height(float value);
+
+  const Vector2f& position() const;
+  const Vector2f& position(const Vector2f& value);
+
+  const Vector2f& size() const;
+  const Vector2f& size(const Vector2f& value);
+
+  inline bool isEmpty() const;
+  inline bool intersects(float x, float y) const;
+
+  inline bool operator==(const ElementBounds& other) const;
+  ElementBounds& operator=(const ElementBounds& other);
+
+  ElementBounds& operator+=(const ElementBounds& other);
+  ElementBounds& operator-=(const ElementBounds& other);
+  ElementBounds operator+(const ElementBounds& other) const;
+  ElementBounds operator-(const ElementBounds& other) const;
+
+  ElementBounds& operator+=(float value);
+  ElementBounds& operator-=(float value);
+  ElementBounds operator+(float value) const;
+  ElementBounds operator-(float value) const;
+
+ private:
+  float xValue;
+  float yValue;
+  float wValue;
+  float hValue;
+  float xEndValue;
+  float yEndValue;
+  Vector2f positionValue;
+  Vector2f sizeValue;
+
+  void recalculateBounds();
+};
+
+}  // namespace ae
