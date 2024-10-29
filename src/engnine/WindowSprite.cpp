157c78dad93bcc7458f54320b1e64d5ca6def597
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-10-29 10:25:10 -0300

    feat: Change WindowSprite to use sf::BlendAlpha without shader

--- src/engnine/WindowSprite.cpp
+++ src/engnine/WindowSprite.cpp
@@ -6,7 +6,6 @@
 #include <SFML/Graphics/Texture.hpp>
 
 #include "engnine/Lists.hpp"
-#include "engnine/Shaders.h"
 
 namespace Eng {
 
@@ -37,33 +36,8 @@ WindowSprite::~WindowSprite()
 void WindowSprite::onRender(sf::RenderTexture& renderTexture)
 {
   renderTexture.setView(view);
-
-  Shaders& shaders = Shaders::Instance();
+  renderTexture.draw(sprite, sf::BlendAlpha);
   renderTexture.display();
-  text = renderTexture.getTexture();
-
-  sf::Vector2u rSize = renderTexture.getSize();
-  sf::Vector2u sSize = sprite.getTexture()->getSize();
-
-  sf::Vector2f size(
-    static_cast<float>(sSize.x) / rSize.x,
-    static_cast<float>(sSize.y) / rSize.y
-  );
-  sf::Vector2f offset(
-    (static_cast<float>(rSize.x - sSize.x) / 2) / rSize.x,
-    (static_cast<float>(rSize.y - sSize.y) / 2) / rSize.y
-  );
-
-  shaders.backTextureBlend->setUniform("offset", offset);
-  shaders.backTextureBlend->setUniform("size", size);
-  shaders.backTextureBlend->setUniform("backTexture", text);
-
-  sf::RenderStates state;
-  state.texture = &text;
-
-  renderTexture.draw(sprite, state);
-  renderTexture.display();
-
   renderTexture.setView(renderTexture.getDefaultView());
 }
 
