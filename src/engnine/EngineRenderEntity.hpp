f353c5d5173e24bcee88cabf3afedc4ce16e0289
Gabriel Gonçalves Martins <gabrielgmartinsbr@gmail.com>
2024-10-29 07:40:29 -0300

    feat: Add EngineRenderEntity base class

---
+++ src/engnine/EngineRenderEntity.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <SFML/Graphics/RenderTexture.hpp>
+
+#include "engnine/IOnRender.h"
+#include "engnine/Lists.hpp"
+
+namespace Eng {
+
+class EngineRenderEntity : public IOnRender {
+ public:
+
+  EngineRenderEntity()
+  {
+    addedToRenderList = false;
+    addToRenderList();
+  }
+
+  ~EngineRenderEntity()
+  {
+    removeToRenderList();
+  }
+
+  virtual void onRender(sf::RenderTexture& renderTexture) = 0;
+
+  virtual bool shouldRender() const = 0;
+
+  virtual int getZIndex() const = 0;
+
+ protected:
+  bool addedToRenderList;
+
+  void addToRenderList()
+  {
+    if (addedToRenderList) return;
+    Lists::Instance().addRenderEntry(this);
+    addedToRenderList = true;
+  }
+
+  void removeToRenderList()
+  {
+    if (!addedToRenderList) return;
+    Lists::Instance().removeRenderEntry(this);
+    addedToRenderList = false;
+  }
+};
+
+}  // namespace Eng
